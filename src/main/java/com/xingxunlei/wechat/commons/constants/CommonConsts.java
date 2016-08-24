418d188f62d06b7f103ec0e83a667d621bb9e5f6
Min.Hsing <xingxunlei@126.com>
2016-08-24 11:13:09 +0800

    change the name of commonconstants class

---
+++ src/main/java/com/xingxunlei/wechat/commons/constants/CommonConsts.java
@@ -0,0 +1,29 @@
+/**
+ * CommonConstants.java
+ * com.xingxunlei.wechat.commons.constants
+ *
+ * Function： 常用静态常量类
+ *
+ *   ver     date      		author
+ * ──────────────────────────────────
+ *   		 2016-8-17 		Simon
+ *
+ * Copyright (c) 2016, 91Bee All Rights Reserved.
+*/
+
+package com.xingxunlei.wechat.commons.constants;
+/**
+ * 常用静态常量类
+ * </br>统一管理常用静态常量
+ *
+ * @author   Simon
+ * @version  
+ * @since    Ver 1.1
+ * @Date	 2016-8-17		上午10:03:41
+ *
+ * @see 	 
+ */
+public class CommonConsts {
+    
+}
+
